2d90ea1f501b62f5e94c5838e4d947ba1e52c1af
Mahmoud Shehata <Shehata@msmo.io>
2020-01-26 10:51:29 +1100

    Start of Episode 3

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
+import { Box, useColorMode } from '@chakra-ui/core';
+
 const index = () => {
+	const { colorMode } = useColorMode();
 	return (
-		<div style={{ textAlign: 'center', fontSize: '1.25rem' }}>
-			Let's Start from here!
-		</div>
+		<Box
+			w='200px'
+			h='20vh'
+			bg={['gray.200', 'gray.300', 'gray.400', 'gray.600']}
+			rounded='full'
+			boxShadow='md'
+		/>
 	);
 };
 
